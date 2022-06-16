8ed7e8d247097e10d20d2b6b1c5132c72c490a17
LahiruRJ <sanlahiru720@gmail.com>
2022-06-16 09:30:00 +0530

    Add country codes

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const selectTag=document.querySelectorAll("select");
 selectTag.forEach(tag=>{
     // console.log(tag);
-    for (const key in object) {
+    for (const country_code in countries) {
+        console.log(countries[country_code]);
+
+        let option=` <option value="${country_code}">${countries[country_code]}</option>`;
+        
        
     }
 })

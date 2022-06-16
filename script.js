8cebadc651de69bed97deac1cf1355038720bc5a
LahiruRJ <sanlahiru720@gmail.com>
2022-06-16 09:42:35 +0530

    Set default language

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const selectTag=document.querySelectorAll("select");
-selectTag.forEach(tag=>{
+selectTag.forEach((tag,id)=>{
   
     for (const country_code in countries) {
+        let selected;
+        if(id==0 && country_code == "en-GB"){
+            selected="selected";
+
+        }else if(id==1 && country_code == "hi-IN"){
+            selected="selected";
+        }
         console.log(countries[country_code]);
 
-        let option=`<option value="${country_code}">${countries[country_code]}</option>`;
+        let option=`<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend",option);
        
     }
